adcd776537b37a551b05e4640e52b970c1a12c07
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-16 12:19:35 -0600

    Finish puntaje view

--- src/views/Dashboard/DashboardPuntajesView.vue
+++ src/views/Dashboard/DashboardPuntajesView.vue
@@ -1,28 +1,24 @@
 <template>
-    <div>
-      <h1 class="text-xl font-bold">Puntajes</h1>
-  
-      <PuntajesTable
-        :puntajeData="puntajeData"
-      />
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, reactive, onMounted } from "vue";
-  import {
-    SwalError,
-    SwalLoading,
-    SwalCustomLoading,
-    SwalDelete,
-    SwalClose,
-    SwalSuccess,
-    SwalWarning,
-  } from "@/components/SwalAlerts/index";
-  import toDoRequest from "@/api/toDoRequests";
-  import PuntajesTable from "@/components/Tables/PuntajesTable.vue";
-  
-  const puntajeData = ref([]);
-  
-  </script>
-  
+  <div>
+    <h1 class="text-xl font-bold">Puntajes</h1>
+
+    <PuntajesTable :puntajeData="puntajeData" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, onMounted } from "vue";
+import {
+  SwalError,
+  SwalLoading,
+  SwalCustomLoading,
+  SwalDelete,
+  SwalClose,
+  SwalSuccess,
+  SwalWarning,
+} from "@/components/SwalAlerts/index";
+import toDoRequest from "@/api/toDoRequests";
+import PuntajesTable from "@/components/Tables/PuntajesTable.vue";
+
+const puntajeData = ref([]);
+</script>
