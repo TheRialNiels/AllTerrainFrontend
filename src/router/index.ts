fa36e4297b561c31abaadbb1b08204a82793dca7
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-16 12:19:12 -0600

    Protect routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,102 +1,165 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 import { useAuth } from "@/stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'Login',
-      component: () => import('@/views/Auth/LoginView.vue')
+      path: "/login",
+      name: "Login",
+      component: () => import("@/views/Auth/LoginView.vue"),
     },
     {
-      path: '/register',
-      name: 'Register',
-      component: () => import('@/views/Auth/RegistrateView.vue')
+      path: "/register",
+      name: "Register",
+      component: () => import("@/views/Auth/RegistrateView.vue"),
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: () => import('@/layouts/DashboardLayout.vue'),
+      path: "/dashboard",
+      name: "Dashboard",
+      component: () => import("@/layouts/DashboardLayout.vue"),
       children: [
         {
-          path: '/dashboard/usuarios',
-          name: 'DashboardUsers',
-          component: () => import('@/views/Dashboard/DashboardUsersView.vue')
+          path: "/dashboard/usuarios",
+          name: "DashboardUsers",
+          component: () => import("@/views/Dashboard/DashboardUsersView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: false,
+            juezRequired: false,
+          },
         },
         {
-          path: '/dashboard/equipos',
-          name: 'DashboardEquipos',
-          component: () => import('@/views/Dashboard/DashboardEquiposView.vue')
+          path: "/dashboard/equipos",
+          name: "DashboardEquipos",
+          component: () => import("@/views/Dashboard/DashboardEquiposView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: false,
+            juezRequired: false,
+          },
         },
         {
-          path: '/dashboard/universidades',
-          name: 'DashboardUniversidades',
-          component: () => import('@/views/Dashboard/DashboardUniversidadesView.vue')
+          path: "/dashboard/universidades",
+          name: "DashboardUniversidades",
+          component: () =>
+            import("@/views/Dashboard/DashboardUniversidadesView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: false,
+            juezRequired: false,
+          },
         },
         {
-          path: '/dashboard/asesores',
-          name: 'DashboardAsesores',
-          component: () => import('@/views/Dashboard/DashboardAsesorView.vue')
+          path: "/dashboard/asesores",
+          name: "DashboardAsesores",
+          component: () => import("@/views/Dashboard/DashboardAsesorView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: false,
+            juezRequired: false,
+          },
         },
         {
-          path: '/dashboard/pruebas',
-          name: 'DashboardPruebas',
-          component: () => import('@/views/Dashboard/DashboardPruebasView.vue')
+          path: "/dashboard/pruebas",
+          name: "DashboardPruebas",
+          component: () => import("@/views/Dashboard/DashboardPruebasView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: false,
+            juezRequired: true,
+          },
         },
         {
-          path: '/dashboard/puntajes',
-          name: 'DashboardPuntajes',
-          component: () => import('@/views/Dashboard/DashboardPuntajesView.vue')
+          path: "/dashboard/puntajes",
+          name: "DashboardPuntajes",
+          component: () =>
+            import("@/views/Dashboard/DashboardPuntajesView.vue"),
+          meta: {
+            adminRequired: true,
+            encargadoRequired: true,
+            juezRequired: false,
+          },
         },
         {
-          path: '/dashboard/encargado',
-          name: 'DashboardEncargado',
-          component: () => import('@/views/Dashboard/DashboardEncargadoView.vue')
+          path: "/dashboard/encargado",
+          name: "DashboardEncargado",
+          component: () =>
+            import("@/views/Dashboard/DashboardEncargadoView.vue"),
+          meta: {
+            adminRequired: false,
+            encargadoRequired: true,
+            juezRequired: false,
+          },
         },
-      ]
-    }
-  ]
-})
-
-router.beforeEach(async (to, from, next) => {
-  const publicPages = ["Login","Register"];
-  const adminPages=["DashboardUsers","DashboardEquipos","DashboardUniversidades","DashboardAsesores","DashboardPruebas","DashboardPuntajes"]
-  const encargadoPages=["DashboardEncargado","DashboardPuntajes"]
-  const juezPages=["DashboardPruebas"]
-  //@ts-ignore
-  const authRequired = !publicPages.includes(to.name);
-  const adminRequired = adminPages.includes(to.name)
-  const encargadoRequired = encargadoPages.includes(to.name)
-  const juezRequired = juezPages.includes(to.name)
-  const loggedIn = await useAuth().currentUser()
-  const role = await useAuth().getRole
-
-  if (authRequired && !loggedIn) {
-    return next('/login')
-  } else {
-    if (loggedIn) {
-      if (adminRequired) {
-        if (role === 'admin') {
-          return next()
-        } else if (role === 'encargado' || role === 'juez') {
-          return next('/dashboard')
-        }
-      }
-
-      if (encargadoRequired) {
-        if (role === 'encargado') {
-          return next()
-        } else if (role === 'admin') {
-          return next('/dashboard/usuarios')
-        } else if (role === 'juez' && !juezRequired) {
-          return next('/user/pruebas')
-        }
-      }
-    }
-
-    next()
-  }
+      ],
+    },
+  ],
 });
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  const publicPages = ["Login", "Register"];
+  const loggedIn = await useAuth().currentUser();
+  const role = useAuth().getRole;
+
+  //@ts-ignore
+  if (!publicPages.includes(to.name) && !loggedIn) {
+    return next("/login");
+  }
+
+  if (loggedIn) {
+    if (
+      to.matched.some((record) => record.meta.adminRequired && record.meta.encargadoRequired)
+    ) {
+      if (role === "admin" || role === "encargado") {
+        return next();
+      } else if (role === "juez") {
+        return next("/dashboard/pruebas");
+      }
+    }
+
+    if (
+      to.matched.some((record) => record.meta.adminRequired && record.meta.juezRequired)
+    ) {
+      if (role === "admin" || role === "juez") {
+        return next();
+      } else if (role === "encargado") {
+        return next("/dashboard/encargado");
+      }
+    }
+
+    if (to.matched.some((record) => record.meta.adminRequired)) {
+      if (role === "admin") {
+        return next();
+      } else if (role === "encargado") {
+        return next("/dashboard/encargado");
+      } else if (role === "juez") {
+        return next("/dashboard/pruebas");
+      }
+    }
+
+    if (to.matched.some((record) => record.meta.encargadoRequired)) {
+      if (role === "encargado") {
+        return next();
+      } else if (role === "admin") {
+        return next("/dashboard/usuarios");
+      } else if (role === "juez") {
+        return next("/dashboard/pruebas");
+      }
+    }
+
+    if (to.matched.some((record) => record.meta.juezRequired)) {
+      if (role === "juez") {
+        return next();
+      } else if (role === "admin") {
+        return next("/dashboard/usuarios");
+      } else if (role === "encargado") {
+        return next("/dashboard/puntajes");
+      }
+    }
+  }
+
+  next();
+});
+
+export default router;
