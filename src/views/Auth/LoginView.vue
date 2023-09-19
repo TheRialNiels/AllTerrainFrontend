59b63c963893c0567a1b42e6e7ee22a4e17a85e9
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-18 23:24:36 -0600

    Add image

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,69 +1,65 @@
 <template>
-  <div class="flex h-screen items-center justify-center">
+  <div class="flex h-screen items-center justify-center bg-[#f7f7f7]">
+    <div class="hidden lg:block lg:basis-1/2 lg:w-1/2 mx-10">
+      <img src="/img/logo.png" alt="allTerrainLogo" class="w-full h-full" />
+    </div>
 
-    <div>
-      <img src="/img/logo.png" alt="allTerrainLogo">
-    </div>  
-
-    <div
-      class="flex flex-col items-center justify-center rounded-lg bg-primary px-10 py-5"
-    >
-      <h1 class="text-xl text-white font-bold">Iniciar sesión</h1>
-
-      <div class="mt-5 w-full">
-        <div class="mb-3">
-          <label for="email" class="mb-2 block font-bold text-white">
-            Correo electrónico
-          </label>
-          <input
-            v-model="form.email"
-            type="email"
-            id="email"
-            class="block w-full rounded-lg border border-white bg-transparent p-2 text-sm text-white placeholder:text-white/90 focus:border-white focus:ring-white"
-            placeholder="Correo electrónico"
-            required
-          />
+    <div class="lg:basis-1/2">
+      <div
+        class="mx-auto lg:w-1/2 flex flex-col items-center justify-center rounded-lg bg-primary px-10 py-5"
+      >
+        <h1 class="text-xl font-bold text-white">Iniciar sesión</h1>
+        <div class="mt-5 w-full">
+          <div class="mb-3">
+            <label for="email" class="mb-2 block font-bold text-white">
+              Correo electrónico
+            </label>
+            <input
+              v-model="form.email"
+              type="email"
+              id="email"
+              class="block w-full rounded-lg border border-white bg-transparent p-2 text-sm text-white placeholder:text-white/90 focus:border-white focus:ring-white"
+              placeholder="Correo electrónico"
+              required
+            />
+          </div>
+          <div>
+            <label for="password" class="mb-2 block font-bold text-white">
+              Contraseña
+            </label>
+            <input
+              v-model="form.password"
+              type="password"
+              id="password"
+              class="block w-full rounded-lg border border-white bg-transparent p-2 text-sm text-white placeholder:text-white/90 focus:border-white focus:ring-white"
+              placeholder="Contraseña"
+              required
+            />
+          </div>
         </div>
-
-        <div>
-          <label for="password" class="mb-2 block font-bold text-white">
-            Contraseña
-          </label>
-          <input
-            v-model="form.password"
-            type="password"
-            id="password"
-            class="block w-full rounded-lg border border-white bg-transparent p-2 text-sm text-white placeholder:text-white/90 focus:border-white focus:ring-white"
-            placeholder="Contraseña"
-            required
-          />
+        <!-- <div class="mt-4 flex w-full justify-end">
+          <p class="cursor-pointer text-sm text-white hover:text-black/50">
+            Olvidaste tu contraseña?
+          </p>
+        </div> -->
+        <div class="mt-5 w-full">
+          <button
+            type="submit"
+            class="w-full rounded-lg bg-white py-2.5 text-sm font-bold text-black transition-all duration-300 ease-in-out hover:bg-black/90 hover:text-white"
+            @click.prevent="login"
+          >
+            Iniciar sesión
+          </button>
+        </div>
+        <div class="mt-5 w-full">
+          <p class="cursor-pointer text-sm text-white">
+            ¿No tienes una cuenta?
+            <a href="/register" class="hover:text-black/50">Registrate aquí</a>
+          </p>
         </div>
       </div>
-
-      <!-- <div class="mt-4 flex w-full justify-end">
-        <p class="cursor-pointer text-sm text-white hover:text-black/50">
-          Olvidaste tu contraseña?
-        </p>
-      </div> -->
-
-      <div class="mt-5 w-full">
-        <button
-          type="submit"
-          class="w-full rounded-lg bg-white py-2.5 text-sm font-bold text-black transition-all duration-300 ease-in-out hover:bg-black/90 hover:text-white"
-          @click.prevent="login"
-        >
-          Iniciar sesión
-        </button>
-      </div>
-
-      <div class="mt-5 w-full">
-        <p class="cursor-pointer text-sm text-white">
-          ¿No tienes una cuenta?
-          <a href="/register" class="hover:text-black/50">Registrate aquí</a>
-        </p>
-      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script setup lang="ts">
--- src/views/Auth/RegistrateView.vue
+++ src/views/Auth/RegistrateView.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="flex h-screen items-center justify-center">
-    <div
-      class="flex flex-col items-center justify-center rounded-lg bg-primary px-20 py-5"
-    >
-      <h1 class="text-xl font-bold text-white">Crear cuenta</h1>
+  <div class="flex h-screen items-center justify-center bg-[#f7f7f7]">
+    <div class="hidden xl:block xl:basis-1/3 lg:w-1/2 mx-10">
+      <img src="/img/logo.png" alt="allTerrainLogo" class="w-full h-full" />
+    </div>
 
-      <AddRegisterForm :form="form" @save-user="saveUser" />
-
-      <div class="mt-4 flex w-full justify-end">
-        <p class="cursor-pointer text-sm text-white">
-          ¿Ya tienes una cuenta?
-          <a href="/login" class="hover:text-black/50">Inicia sesión aquí</a>
-        </p>
-      </div>
-
-      <div class="mt-10 w-full">
-        <button
-          type="submit"
-          class="w-full rounded-lg bg-white py-2.5 text-sm font-bold text-black transition-all duration-300 ease-in-out hover:bg-black/90 hover:text-white"
-          @click.prevent="saveUser"
-        >
-          Registrate
-        </button>
+    <div class="xl:basis-2/3">
+      <div
+        class="mx-auto xl:w-1/2 flex flex-col items-center justify-center rounded-lg bg-primary px-20 py-5"
+      >
+        <h1 class="text-xl font-bold text-white">Crear cuenta</h1>
+        <AddRegisterForm :form="form" @save-user="saveUser" />
+        <div class="mt-4 flex w-full justify-end">
+          <p class="cursor-pointer text-sm text-white">
+            ¿Ya tienes una cuenta?
+            <a href="/login" class="hover:text-black/50">Inicia sesión aquí</a>
+          </p>
+        </div>
+        <div class="mt-10 w-full">
+          <button
+            type="submit"
+            class="w-full rounded-lg bg-white py-2.5 text-sm font-bold text-black transition-all duration-300 ease-in-out hover:bg-black/90 hover:text-white"
+            @click.prevent="saveUser"
+          >
+            Registrate
+          </button>
+        </div>
       </div>
     </div>
   </div>
